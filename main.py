5aa82367449fc96ccbf60d166a26709effb4ad76
Elen2056 <eltacombis@gmail.com>
2024-11-07 15:16:30 +0300

    Добавлена функция сохранения в формате json

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 from tkinter import ttk
 import pyperclip
+import json
+import os
+
+history_file = "upload_history.json"
+
+
+def save_history(file_path, download_link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, "r") as file:
+            history = json.load(file)
+
+    history.append({"file_path": os.path.basename(file_path), "download_link": download_link})
+
+    with open(history_file, "w") as file:
+        json.dump(history, file, indent=4)
+
+
 def upload():
     try:
         filepath = fd.askopenfilename()
@@ -18,6 +36,7 @@ def upload():
                     entry.delete(0, END)
                     entry.insert(0, link)
                     pyperclip.copy(link)
+                    save_history(filepath, link)
                     mb.showinfo("Папка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена")
                 else:
                     raise ValueError("Не удалось получить ссылку для скачивания")
@@ -37,4 +56,5 @@ entry = ttk.Entry()
 entry.pack()
 
 window.mainloop()
+
 
